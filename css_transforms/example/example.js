05ade9014bb944909a4648184d34ac32cbb52740
Jamie Padovano <jp1483@messiah.edu>
2024-01-30 20:52:57 -0500

    finished assignment

--- css_transforms/example/example.js
+++ css_transforms/example/example.js
@@ -1,36 +1,37 @@
+// Jamie Padovano
+
 "use strict";
 
 class Square {
-  constructor() {
+  constructor(direction = "horizontal", color = "blue", size = "50px") {
     this.div = document.createElement("div");
     document.querySelector("body").appendChild(this.div);
-    this.div.style.width = this.div.style.height = "150px";
-    this.div.style.backgroundColor = "rgb(250, 200, 120)";
-    this.div.style.paddingTop = "10px";
-    this.div.style.textAlign = "center";
-    this.div.innerHTML = "Hello, World!";
-    
-    this.div.style.transformOrigin = "center center 0";
-  }
+    this.div.className = direction;
+    this.div.style.width = this.div.style.height = this.div.style.zIndex = size;
+    this.div.style.backgroundColor = color;
 
-  updateTransform(frameCount) {
-    const angle = 100 * Math.sin(frameCount / 60);
-    this.div.style.transform = "rotate(" + angle + "deg)";
+    // make speed scale with square size - bigger = faster
+    const speed = 17 - Math.floor((parseInt(size) / 230) * 13);
+    this.div.style.animationDuration = speed + "s";
+    
+    const xPos = Math.random() * window.innerWidth;
+    const yPos = Math.random() * window.innerHeight;
+    if (direction == "horizontal") this.div.style.top = `${yPos}px`;
+    else this.div.style.left = `${xPos}px`;
   }
 }
 
 function main() {
-  let squares = [ new Square(), new Square(), new Square()];
-  let frameCount = 0;
-  
-  function animate() {
-    squares.forEach(s => s.updateTransform(frameCount));
-    frameCount++;
-    
-    requestAnimationFrame(animate);
-  }
-  
-  requestAnimationFrame(animate);
+  let squares = [];
+  const colorList = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+  setInterval(() => {
+    const direction = Math.random() < 0.5 ? "horizontal" : "vertical";
+    const color = colorList[Math.floor(Math.random() * colorList.length)];
+    const size = Math.random() * 200 + 30 + "px";
+
+    squares.push(new Square(direction, color, size));
+  }, 200)
 }
 
 window.onload = main;
